cee8d61b161483ac8942c74affa878016b77b026
ElasticBottle <winstonyeo99@yahoo.com>
2023-06-24 05:22:52 -0400

    chore(cross-chain-payment-core): update barrel file

--- packages/alttransfer-cross-chain-payment-core/src/index.tsx
+++ packages/alttransfer-cross-chain-payment-core/src/index.tsx
@@ -1,1 +1,4 @@
-export {};
+export { AltTransferCrossChainSdk } from "./lib/sdk";
+export type { AltTransferCrossChainSdkConstructorArgs } from "./lib/sdk";
+export type { SupportedChainIds } from "./types/SupportedChainIds";
+export type { TokenInfo } from "./types/TokenInfo";
