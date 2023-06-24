245453a5889b97e82106c9dec43322205c706388
ElasticBottle <winstonyeo99@yahoo.com>
2023-06-24 05:20:55 -0400

    feat(cross-chain-payment-core): add stub for needed functions

---
+++ packages/alttransfer-cross-chain-payment-core/src/types/TokenInfo.ts
@@ -0,0 +1,13 @@
+import { SupportedChainIds } from "./SupportedChainIds";
+
+export type TokenInfo = {
+  address: string;
+  decimals: number;
+  name: string;
+  symbol: string;
+  balance: string;
+  tokenUri?: string;
+  chainId: SupportedChainIds;
+  balanceUsdValueCents: string;
+  tokenExchangeUsdValueCents: string;
+};
