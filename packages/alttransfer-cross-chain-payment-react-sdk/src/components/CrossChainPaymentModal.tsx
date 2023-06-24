e4dcfb294ce86e08f26b7f7dfde44bd3afcc69ca
Angela Du <angela.du@jam3.com>
2023-06-24 12:24:59 -0400

    Revert "moved over modal code from private repo"
    This reverts commit 098a9ecae886ee05ac9e7d2a5b225a05f0764a9f.

--- packages/alttransfer-cross-chain-payment-react-sdk/src/components/CrossChainPaymentModal.tsx
+++ packages/alttransfer-cross-chain-payment-react-sdk/src/components/CrossChainPaymentModal.tsx
@@ -3,23 +3,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import * as React from "react";
 import { useDestinationInfo } from "../hooks/useDestinationInfo";
 import { CrossChainPaymentProvider } from "./CrossChainPaymentContext";
-import HomePage from "./crossChainPaymentModal/homepage";
-import SelectToken from "./crossChainPaymentModal/selectToken";
-import ModifyWallet from "./crossChainPaymentModal/modifyWallet";
-import SelectChain from "./crossChainPaymentModal/selectChain";
-import * as Dialog from "@radix-ui/react-dialog";
-import "./crossChainPaymentModal/defaultModal.css"
 
 const queryClient = new QueryClient();
 
-export enum pages {
-  HomeScreen = "homeScreen",
-  SelectToken = "selectToken",
-  SelectChain = "selectChain",
-  ModifyWallet = "modifyWallet",
-  ConfirmPayment = "confirmPayment",
-  PaymentSubmit = "paymentSubmit",
-}
 export type AltTransferCrossChainPaymentModalProps = {
   children: React.ReactNode;
 } & AltTransferCrossChainSdkConstructorArgs;
@@ -34,47 +20,15 @@ export function AltTransferCrossChainPaymentModal(
         getItemPrice={props.getItemPrice}
         getRecipientAddress={props.getRecipientAddress}
       >
-        <PaymentModal transferAmount="123">{props.children}</PaymentModal>
+        <PaymentModal>{props.children}</PaymentModal>
       </CrossChainPaymentProvider>
     </QueryClientProvider>
   );
 }
 
-interface modalProps {
-  children: React.ReactNode;
-  transferAmount: string;
-}
-
-const PaymentModal: React.FC<modalProps> = (props) => {
+function PaymentModal({ children }: { children: React.ReactNode }) {
   const { isLoadingDestinationInfo, destinationInfo, destinationError } =
     useDestinationInfo();
-  const [currentScreen, setCurrentScreen] = React.useState(pages.HomeScreen);
-  const [curChain, setCurChain] = React.useState("Fantom");
-
-  
-  const renderPage = () => {
-    switch (currentScreen) {
-      case pages.HomeScreen:
-        return (
-          <HomePage setCurrentScreen={setCurrentScreen} curChain={curChain} />
-        );
-      case pages.SelectToken:
-        return <SelectToken setCurrentScreen={setCurrentScreen} />;
-      case pages.ModifyWallet:
-        return <ModifyWallet setCurrentScreen={setCurrentScreen} />;
-      case pages.SelectChain:
-        return (
-          <SelectChain
-            setCurrentScreen={setCurrentScreen}
-            currentChain={curChain}
-            setCurrentChain={setCurChain}
-          />
-        );
-      default:
-        <div></div>;
-    }
-  };
-
 
   console.log("destinationInfo", destinationInfo);
   console.log("isLoadingDestinationInfo", isLoadingDestinationInfo);
@@ -84,12 +38,5 @@ const PaymentModal: React.FC<modalProps> = (props) => {
     return <div>Something went wrong fetching payment information</div>;
   }
 
-return (
-<Dialog.Root>
-  <Dialog.Trigger asChild>{props.children}</Dialog.Trigger>
-  <Dialog.Portal>
-    <Dialog.Overlay className="DialogOverlay" />
-    {renderPage()}
-  </Dialog.Portal>
-</Dialog.Root>);
+  return <div>{children}</div>;
 }
