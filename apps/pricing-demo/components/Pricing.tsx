206ec114cfa36a11a3e7bcbbe22e47319601179b
Maanav Rajesh <maanavrajesh13@gmail.com>
2023-06-25 06:46:54 -0400

    supa hosting

--- apps/pricing-demo/components/Pricing.tsx
+++ apps/pricing-demo/components/Pricing.tsx
@@ -148,7 +148,7 @@ export default function Pricing({
                     config={{
                       ChainAPIs: {
                         "0x1":
-                        "https://solemn-holy-layer.quiknode.pro/859bd0ec14bf3fa09445f2ceddbd9cef5c5f436c/",
+                          "https://solemn-holy-layer.quiknode.pro/859bd0ec14bf3fa09445f2ceddbd9cef5c5f436c/",
                         "0x89":
                           "https://red-flashy-glade.matic.quiknode.pro/06e9538c594d0769ad9a0c3fd5ab4b883442619f/",
                       },
@@ -156,19 +156,19 @@ export default function Pricing({
                     }}
                     getItemPrice={async () => {
                       return Promise.resolve({
-                        amount: "100000",
+                        amount: "100000000000000",
                         chainId: "0xa",
-                        isNative: false,
-                        tokenAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+                        isNative: true,
+                        // tokenAddress: "0x7f5c764cbc14f9669b88837ca1490cca17c31607",
                       });
                     }}
                     getRecipientAddress={async () => {
                       return Promise.resolve({
-                        address: "0x123",
+                        address: "0xb3E9C57fB983491416a0C77b07629C0991c3FD59",
                       });
                     }}
                     text={{
-                      brandName: "AltTransfer",
+                      brandName: "Supa Hosting",
                     }}
                   >
                     <Button
